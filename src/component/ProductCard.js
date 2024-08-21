d77e9b9ff7aa87b2d0f9ae086e698ebb1d39dd94
JeongChan2 <jcstar55@gmail.com>
2024-08-21 22:34:23 +0900

    feat: 1~5

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img alt='none' src='https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Ffe%2Fa4%2Ffea4dc2da7323ad083d8aae267f452563dce5568.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bkids_baby_boy_clothing_tshirtsshirts_tshirts%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url[file:/product/fullscreen]'/>
+      <div>Conscious choice</div>
+      <div>벨티드 트윈 코트</div>
+      <div>₩99900</div>
+      <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
